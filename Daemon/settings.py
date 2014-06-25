2d1386c7e9c765c96cc2d271b7ae540b0f5d3a7b
root <root@saloon.tf>
2014-06-25 10:52:37 -0400

    Remove steamapi key from the settings.py

--- Daemon/settings.py
+++ Daemon/settings.py
@@ -3,10 +3,6 @@ steam = {
   'guard': {
     'interval': 5,
     'retries': 3
-  },
-  'trade': {
-    'incorrectHandler': 'declineOffer'
-  },
-  'api': "Enter your api key here"
+  }
 }
 database = 'postgresql://user:password@localhost:port/Database'
